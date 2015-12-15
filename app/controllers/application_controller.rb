4df1914ffcc62e6bc2fdcecb5f674eb50fd2657e
Amin Rezapour <aminrezapour@gmail.com>
2015-12-14 16:51:16 -0800

    Add extra fields to user model
    Make the views visible by running $rails g devise:views users
    
    Uncomment config.scoped_views = true in config/initializers/devise.rb
    and restart server.
    
    Add a migration file to user model by $rails g migration AddNameToUsers
    name:string
    
    Add how many variables to the created migration file.
    
    To be able to save, add this to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(current_user)
     user_path(current_user)
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :is_female, :date_of_birth) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+  end
+
 end
